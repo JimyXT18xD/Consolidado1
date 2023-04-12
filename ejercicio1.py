ee6a0fc5e70474e1fdfb091b70091983eb4bac4a
Jimy Moises <74760713@continental.edu.pe>
2023-04-12 16:40:49 -0500

    Codigo mejorado con Excepciones

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,15 +1,26 @@
 def suma(a, b):
-        return a + b
+    return a + b
 
 def resta(a, b):
-        return a - b
+    return a - b
 
 def multiplicacion(a, b):
-        return a * b
+    return a * b
 
 def division(a, b):
-        return a / b
+    if b == 0:
+        raise ValueError("Error: Ingrese un numero natural")
+    return a / b
 
+def obtener_numero(entrada, mensaje):
+        while True:
+        try:
+            numero = float(input(entrada))
+            break
+        except ValueError:
+            print("Error: Entrada incorrecta, solo se permite numeros")
+    return numero
 
-num1 = float(input("Ingrese el primer número: "))
-num2 = float(input("Ingrese el segundo número: "))
+try:
+    num1 = obtener_numero("Ingrese el primer número: ", "Error: Entrada incorrecta, solo se permite numeros")
+    num2 = obtener_numero("Ingrese el segundo número: ", "Error: Entrada incorrecta, solo se permite numeros")
