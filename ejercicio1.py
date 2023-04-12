80846bb28d8e5303a0c830e138cd940034ce310d
Roy Farfan <71960412@continental.edu.pe>
2023-04-12 16:43:12 -0500

    Ejercicio completo con excepciones

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,34 +1,55 @@
 def suma(a, b):
-        return a + b
+    return a + b
 
 def resta(a, b):
-        return a - b
+    return a - b
 
 def multiplicacion(a, b):
-        return a * b
+    return a * b
 
 def division(a, b):
-        return a / b
+    if b == 0:
+        raise ValueError("Error: Ingrese un numero natural")
+    return a / b
 
+def obtener_numero(entrada, mensaje):
+        while True:
+        try:
+            numero = float(input(entrada))
+            break
+        except ValueError:
+            print("Error: Entrada incorrecta, solo se permite numeros")
+    return numero
 
-num1 = float(input("Ingrese el primer número: "))
-num2 = float(input("Ingrese el segundo número: "))
+try:
+    num1 = obtener_numero("Ingrese el primer número: ", "Error: Entrada incorrecta, solo se permite numeros")
+    num2 = obtener_numero("Ingrese el segundo número: ", "Error: Entrada incorrecta, solo se permite numeros")
 
-print("OPERACIONES:")
-print("1. Suma")
-print("2. Resta")
-print("3. Multiplicación")
-print("4. División")
+    print("OPERACIONES:")
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicación")
+    print("4. División")
 
-opcion = input("Que operación desea aplicar (1/2/3/4): ")
+        while True:
+        opcion = input("Que operación desea aplicar (1/2/3/4): ")
+        if opcion in ["1", "2", "3", "4"]:
+            break
+        else:
+            print("Error: Entrada incorrecta, solo se permite numeros")
 
-if opcion == "1":
-    resultado = suma(num1, num2)
-elif opcion == "2":
-    resultado = resta(num1, num2)
-elif opcion == "3":
-    resultado = multiplicacion(num1, num2)
-else:
-    resultado = division(num1, num2)
+       if opcion == "1":
+        resultado = suma(num1, num2)
+    elif opcion == "2":
+        resultado = resta(num1, num2)
+    elif opcion == "3":
+        resultado = multiplicacion(num1, num2)
+    else:
+        resultado = division(num1, num2)
 
-print("Resultado: ", resultado)
+       print("Resultado: ", resultado)
+
+except ValueError as e:
+    print(str(e))
+except Exception as e:
+    print("Error: ", str(e))
