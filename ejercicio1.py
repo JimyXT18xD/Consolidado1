8953b59bfa79a28873b7eebd5ebb7df4e60950ac
Jimy Moises <74760713@continental.edu.pe>
2023-04-12 16:20:54 -0500

    Esta es la primera version basica del codigo Primer Ejercicio

---
+++ ejercicio1.py
@@ -0,0 +1,15 @@
+def suma(a, b):
+        return a + b
+
+def resta(a, b):
+        return a - b
+
+def multiplicacion(a, b):
+        return a * b
+
+def division(a, b):
+        return a / b
+
+
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
